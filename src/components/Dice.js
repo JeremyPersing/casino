92e8b0854dcfc93bcb5c9c7fac236324851a3668
Jeremy Persing <jpersing1999@gmail.com>
2020-11-29 20:55:30 -0800

    Dice logic updated and Interface integrated

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,71 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDice from 'react-dice-complete';
 import 'react-dice-complete/dist/react-dice-complete.css';
 
-const Dice = () => {
-    // Function that generates a random number between 1 and 6
-    const rollDice = () => {
-        let roll = Math.floor(Math.random() * 6 + 1);
-        console.log(roll);
-        return roll;
+
+class Dice extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rolledNum: ''
+        }
+        // Binding methods
+        this.rollAll = this.rollAll.bind(this)
+        this.compareSingle = this.compareSingle.bind(this);
+        this.rollDoneCallback = this.rollDoneCallback.bind(this);
+        this.compareBroad = this.compareBroad.bind(this);
     }
 
-    // Function that compares players choice to the dice roll
-    const outcome = (pick) => {
-        console.log('Players pick: ' + pick);
-        let diceRoll = rollDice();
+    // Rolls the dice
+    rollAll() {
+        this.reactDice.rollAll()
+    }
+    
+    // Sets the rolled number once the dice is done spinning
+    rollDoneCallback(num) {
+        this.setState({rolledNum: num});
+    }
 
-        if (pick === diceRoll) {
-            console.log('You won')
+    // Compares when the player made a bet on a single number
+    compareSingle(num) {
+        this.rollAll();
+        // Set the timeout to allow for the rolledNum state to be set
+        setTimeout(() => {this.determineOutcome1(this.state.rolledNum, num)}, 1000);
+    }
+
+    // Determines the outcome on a single number bet
+    determineOutcome1(rolledNum, guessedNum) {
+        console.log('Guessed: ' + guessedNum);
+        console.log('Rolled: ' + rolledNum);
+        if (rolledNum === guessedNum) {
+            console.log('You Win, x6 profit')
         }
         else {
-            console.log('You Lost');
+            console.log('You lose');
         }
     }
 
-    // Function that gives the player a 50/50 chance
-    const outcomeTwo = (pick) => {
-        let diceRoll = rollDice();
+    // Compares when the player made a 50/50 bet 
+    compareBroad(num) {
+        this.rollAll();
+        // Set the timeout to allow for the rolledNum state to be set
+        setTimeout(() => {this.determineOutcome2(this.state.rolledNum, num)}, 1000);
+    }
 
-        if (pick === 3 && diceRoll <= 3) {
-            console.log('You Won');
-        }
-        else if (pick === 4 && diceRoll >= 4) {
-            console.log('You Won');
+    // Determines the outcome on a 50/50 bet
+    determineOutcome2(rolledNum, guessedNum) {
+        console.log("Rolled: " + rolledNum);
+        console.log('Guessed: ' + guessedNum);
+        if ((rolledNum <= 3 && guessedNum === 3) || (rolledNum >= 4 && guessedNum === 4)) {
+            console.log('You win 2x')
         }
         else {
-            console.log('You lost');
+            console.log("You lose");
         }
+        
     }
 
-    const rollDoneCallBack = (num) => {
-        console.log(`You rolled a ${num}`)
-    }
-
-
-
-    return (
-        <div>
-            <h1>Dice Roll</h1>
+    render() {
+        return (
+          <div>
             <ReactDice
-            numDice={2}
-            rollDone={rollDoneCallBack}
-            defaultRoll={3}
-            outline={true}
-            faceColor={'#BDBCBD'}
-            dotColor={'#333333'}
-            ></ReactDice>
-            <button >1</button>
-            <button onClick={() => {outcome(2)}}>2</button>
-            <button onClick={() => {outcome(3)}}>3</button>
-            <button onClick={() => {outcome(4)}}>4</button>
-            <button onClick={() => {outcome(5)}}>5</button>
-            <button onClick={() => {outcome(6)}}>6</button>
-            <button onClick={() => {outcomeTwo(3)}}>3 or lower</button>
-            <button onClick={() => {outcomeTwo(4)}}>4 or higher</button>
-        </div>
-    )
-} 
+              numDice={1}
+              defaultRoll={3}
+              outline={true}
+              faceColor={'#BDBCBD'}
+              dotColor={'#333333'}
+              dieSize={120}
+              rollDone={this.rollDoneCallback}
+              ref={dice => this.reactDice = dice}
+              disableIndividual={true}
+              rollTime={1}
+            />
+            <button onClick={() => {this.compareSingle(1)}}>1</button>
+            <button onClick={() => {this.compareSingle(2)}}>2</button>
+            <button onClick={() => {this.compareSingle(3)}}>3</button>
+            <button onClick={() => {this.compareSingle(4)}}>4</button>
+            <button onClick={() => {this.compareSingle(5)}}>5</button>
+            <button onClick={() => {this.compareSingle(6)}}>6</button>
+            <button onClick={() => {this.compareBroad(3)}}>3 or lower</button>
+            <button onClick={() => {this.compareBroad(4)}}>4 or higher</button>
+          </div>
+        )
+      }
+}
+
 
 
 export default Dice;
