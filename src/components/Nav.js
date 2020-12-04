d00129398618a11ac6f2039d70740d71a2938c40
Jeremy Persing <jpersing1999@gmail.com>
2020-12-03 23:20:15 -0800

    Added Bootstrap and styled Nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,32 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Nav = (props) => {
     return (
-        <div>
-            <li><NavLink to='/' exact>Home</NavLink></li>
-            <li><NavLink to='/blackjack' exact>Blackjack</NavLink></li>
-            <li><NavLink to='/coinflip' exact>Coin Flip</NavLink></li>
-            <li><NavLink to='/dice' exact>Dice Roll</NavLink></li>
-            <li><NavLink to='/crash' exact>Crash</NavLink></li>
-            <li><NavLink to='/roulette' exact>Roulette</NavLink></li>
-            <p><img src={process.env.PUBLIC_URL + '/images/bitcoinlogo.png'} alt='Coins Logo' style={{height: '25px'}}></img>{parseFloat(props.coins).toFixed(2)}</p>
+        <div class='d-flex justify-content-center'> 
+            <ul class='nav nav-tabs'>
+                <li class='nav-item'>
+                    <Link class='nav-link' to='/' exact>Home</Link>
+                </li>
+                <li class='nav-item'>
+                    <Link class='nav-link' to='/blackjack' exact>Blackjack</Link>
+                </li>
+                <li class='nav-item'>
+                    <Link class='nav-link' to='/coinflip' exact>Coin Flip</Link>
+                </li>
+                <li class='nav-item'>
+                    <Link class='nav-link' to='/dice' exact>Dice Roll</Link>
+                </li>
+                <li class='nav-item'>
+                    <Link class='nav-link' to='/crash' exact>Crash</Link>
+                </li>
+                <li class='nav-item'>
+                    <Link class='nav-link' to='/roulette' exact>Roulette</Link>
+                </li>
+                <li class='mr-auto mt-2'>
+                <img src={process.env.PUBLIC_URL + '/images/bitcoinlogo.png'} alt='Coins Logo' style={{height: '25px'}}></img>
+                {parseFloat(props.coins).toFixed(2)}
+                </li>
+            </ul>
         </div>
     )
 }
