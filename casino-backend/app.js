ed23561de672bdc5d244c21c3946d248cd0a611d
Jeremy Persing <jpersing1999@gmail.com>
2021-01-03 15:26:28 -0800

    potential fixes

--- casino-backend/app.js
+++ casino-backend/app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv/config');
+const port = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(cors());
@@ -15,24 +17,24 @@ const userRoute = require('./routes/users');
 const loginRoute = require('./routes/login')
 const userNameRoute = require('./routes/username');
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 // Middleware for route
 app.use('/user', userRoute);
 app.use('/login', loginRoute);
 app.use('/username', userNameRoute);
 
+//Serve the static react files
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req,res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+// Connect to DB
 mongoose.connect(
   process.env.DB_CONNECTION,
-  { useNewParser: true }, () => {
+  { useNewParser: true, useUnifiedTopology: true, useFindAndModify: false}, () => {
     console.log('You are connected to the DB');
   }
 
 )
 
-app.listen(process.env.PORT || 5000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(port);
